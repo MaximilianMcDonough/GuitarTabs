833a8f76e0f3dc95943572593cfb9d870c1026b6
Maximilian McDonough <maxmcdonough35@gmail.com>
2023-06-14 18:03:35 -0700

    models added

---
+++ models.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 14 17:44:38 2023
+
+@author: max
+"""
+import numpy as np
+from sklearn.ensemble import RandomForestRegressor
+import librosa
+
+
+TAB_FREQUNCIES = []
+TAB_INDEXS = []
+
+
+class test_model():
+    def __init__(self):
+        pass
+    
+    def trian(self, X, y):
+        pass
+    
+    def predict(self, X):
+        pass
+
+
--- test.py
+++ test.py
@@ -6,3 +6,11 @@ Created on Wed Jun 14 16:17:14 2023
 @author: max
 """
 
+def main():
+    pass
+
+
+
+
+if __name__ == '__main__':
+    main()
